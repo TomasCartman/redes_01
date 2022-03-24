ed2a7dee1cc8d0a073f43db234a54958dc82b0ab
Tomas Cartman <marco12y@outlook.com>
2022-03-23 21:22:04 -0300

    change way that server and trash works to communicate

---
+++ server.py
@@ -0,0 +1,141 @@
+import socket
+import select
+import time
+
+HOST = ''
+PORT = 50007
+CHUNK_SIZE = 2048
+
+
+class Server:
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = list()
+        self.readers = [self.server_socket]
+        self.writers = list()
+
+    def prepare_for_connection(self):
+        self.server_socket.bind((HOST, PORT))
+        self.server_socket.setblocking(False)
+        self.server_socket.listen()
+        print(f'Listing to port {PORT}')
+
+    def run(self):
+        try:
+            while True:
+                time.sleep(1.5)
+                readable, writeable, exceptional = select.select(self.readers,  self.writers, self.readers, 0.5)
+
+                for s in readable:
+                    try:
+                        if s == self.server_socket:
+                            client_socket, address = s.accept()
+                            client_socket.setblocking(False)
+                            print(f'Connection from: {address[0]}:{address[1]}')
+                            self.readers.append(client_socket)
+                        else:
+                            data = s.recv(CHUNK_SIZE)
+                            if data:
+                                print('Recv')
+                                self.writers.append(s)
+                            else:
+                                pass
+                                #s.close()
+                                #self.readers.remove(s)
+
+                    except Exception as err:
+                        self.readers.remove(s)
+                        s.close()
+                        print(f'An error occurred: {err}')
+                    finally:
+                        pass
+
+                for s in writeable:
+                    try:
+                        s.send(bytes('send', "utf-8"))
+                        print('Sending')
+                        self.writers.remove(s)
+                    except Exception as err:
+                        print(f'An error occurred: {err}')
+                    finally:
+                        pass
+
+                for s in exceptional:
+                    pass
+
+        except KeyboardInterrupt:
+            print('Keyboard interrupt')
+
+
+'''
+    def accept_connection(self):
+        client_socket, address = self.server_socket.accept()
+        i = 0
+        while True:
+            data = client_socket.recv(CHUNK_SIZE)
+            print(f'{i} -> {data.decode("utf-8")}')
+            #if i % 3 == 0:
+            client_socket.sendall(bytes(f'{i}', "utf-8"))
+            i += 1
+            time.sleep(2)
+        print(f'Client connected: {address[0]}:{address[1]}')
+        self.clients.append(client_socket)
+
+    def receive_from_trashes(self):
+        pass
+        #for client_socket in self.clients:
+        #    print(client_socket)
+        #while True:
+        #    data = self.
+'''
+
+
+server = Server()
+server.prepare_for_connection()
+server.run()
+server.server_socket.close()
+
+'''
+def accept_connection(s):
+    client_socket, address = s.accept()
+    with client_socket:
+        print(f'Connected by {address}')
+        while True:
+            data = client_socket.recv(CHUNK_SIZE)
+            if not data:
+                print(f'Disconnected by {address}')
+                break
+            print(data.decode("utf-8"))
+            client_socket.sendall(b'Message was received')
+    s.close()
+
+
+try:
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((HOST, PORT))
+            s.listen()
+            print('Server started')
+            accept_connection(s)
+        except KeyboardInterrupt:
+            print('Caught KeyboardInterrupt')
+            s.close()
+except:
+    pass
+'''
+""""
+    conn, addr = s.accept()
+    with conn:
+        print(f'Connected by {addr}')
+        try:
+            while True:
+                data = conn.recv(CHUNK_SIZE)
+                if data:
+                    print(data.decode("utf-8"))
+                    conn.sendall(b'Message was received')
+        except KeyboardInterrupt:
+            print('Caught KeyboardInterrupt')
+            s.close()
+    s.close()
+"""
--- trash.py
+++ trash.py
@@ -1,9 +1,78 @@
 import socket
+import select
+import time
 
 HOST = '127.0.0.1'
 PORT = 50007
 CHUNK_SIZE = 2048
 
+
+class Trash:
+    def __init__(self):
+        self.trash_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.inputs = []
+        self.outputs = [self.trash_socket]
+
+    def connect(self):
+        try:
+            self.trash_socket.connect((HOST, PORT))
+            print(f'Connect at {HOST}:{PORT}')
+            self.trash_socket.setblocking(False)
+        except Exception as err:
+            print('An error occurred while trying to connect to the server: ', err)
+
+    # A check must take place (is the socket connected?) before trying to disconnect | Not using
+    def disconnect(self):
+        self.trash_socket.close()
+
+    def send_message_to_server(self, s, message):
+        s.sendall(bytes(message, "utf-8"))
+        print('Sending')
+        self.outputs.remove(s)
+        self.inputs.append(s)
+
+    def receive_message_from_server(self, s):
+        try:
+            server_response = s.recv(CHUNK_SIZE)
+            self.inputs.remove(s)
+            self.outputs.append(s)
+            if server_response:
+                return server_response.decode("utf-8")
+        except Exception as err:
+            print('An error occurred while trying to receive a message from the server: ', err)
+            self.disconnect()
+
+    def run(self):
+        try:
+            while True:
+                time.sleep(1.5)
+                readable, writeable, exceptional = select.select(self.inputs, self.outputs, self.inputs, 0.5)
+
+                for s in writeable:
+                    self.send_message_to_server(s, 'Testing...\n')
+
+                for s in readable:
+                    data = self.receive_message_from_server(s)
+                    print('recv')
+
+                for s in exceptional:
+                    pass
+        except KeyboardInterrupt:
+            print('Keyboard interrupt')
+
+
+t = Trash()
+t.connect()
+t.run()
+t.disconnect()
+# t.connect()
+# t.send_message_to_server(bytes('Testing...', "utf-8"))
+# response = t.receive_message_from_server()
+# t.disconnect()
+# print(response)
+
+
+'''
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     try:
@@ -20,3 +89,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     finally:
         s.close()
     s.close()
+'''
